f2f4ba17ac07f324b1b6be2e5015a0a4a84ea70b
Zarikato <91799879+Zarikato@users.noreply.github.com>
2022-01-24 18:14:47 +0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,26 +1,27 @@
 import pygame
-import pygame_menu
-from typing import Tuple, Any
 
-pygame.init()
-surface = pygame.display.set_mode((600, 400))
+class game():
+    def rungame():
+        pygame.init()
+        pygame.display.set_caption('Движущийся круг 2')
+        size = width, height = 800, 400
+        screen = pygame.display.set_mode(size)
 
-
-def set_difficulty(selected: Tuple, value: Any):
-    print(f'Set difficulty to {selected[0]} ({value})')
-
-
-def start_the_game():
-    global user_name
-    print(f'{user_name.get_value()}, Do the job here!')
-    # если кинуть что-нибудь сюда, оно откроется в отдельном окне, тут будет основная игра
-
-
-menu = pygame_menu.Menu('Space Invaders', 600, 400,
-                        theme=pygame_menu.themes.THEME_DARK)
-
-user_name = menu.add.text_input('Имя:', default=' ')
-menu.add.selector('Сложность игры: ', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
-menu.add.button('Играть!', start_the_game)
-menu.add.button('Выход', pygame_menu.events.EXIT)
-menu.mainloop(surface)
+        running = True
+        x_pos = 0
+        v = 50  # пикселей в секунду
+        fps = 60
+        clock = pygame.time.Clock()
+        while running:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+            screen.fill((0, 0, 0))
+            pygame.draw.circle(screen, (255, 0, 0), (int(x_pos), 200), 20)
+            x_pos += v / fps
+            if x_pos >= 800:
+                x_pos = 0
+            clock.tick(fps)
+            pygame.display.flip()
+        pygame.quit()
+       
