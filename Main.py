ded5f6669d6137f7b70aac4ef52522ac0e5dd573
Andrey <abelov896@gmail.com>
2022-01-24 16:34:22 +0400

    import menu

--- Main.py
+++ Main.py
@@ -1,1 +1,26 @@
-# test
+import pygame
+import pygame_menu
+from typing import Tuple, Any
+
+pygame.init()
+surface = pygame.display.set_mode((600, 400))
+
+
+def set_difficulty(selected: Tuple, value: Any):
+    print(f'Set difficulty to {selected[0]} ({value})')
+
+
+def start_the_game():
+    global user_name
+    print(f'{user_name.get_value()}, Do the job here!')
+    # если кинуть что-нибудь сюда, оно откроется в отдельном окне, тут будет основная игра
+
+
+menu = pygame_menu.Menu('Space Invaders', 600, 400,
+                        theme=pygame_menu.themes.THEME_DARK)
+
+user_name = menu.add.text_input('Имя:', default=' ')
+menu.add.selector('Сложность игры: ', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+menu.add.button('Играть!', start_the_game)
+menu.add.button('Выход', pygame_menu.events.EXIT)
+menu.mainloop(surface)
