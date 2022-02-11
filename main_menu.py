38f6eab75dde081f46062c8a25e9cc63728d7c7a
Larom4ik <76567719+Larom4ik@users.noreply.github.com>
2022-02-11 21:02:50 +0400

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -3,20 +3,21 @@ import pygame_menu
 from pygame_menu import sound
 from typing import Tuple, Any
 from space import Hp
+from Sound import Mixers
 
 pygame.init()
 pygame.display.set_caption('Space Invaders')
-programIcon = pygame.image.load('data/icon.png')
+programIcon = pygame.image.load('icon.png')
 pygame.display.set_icon(programIcon)
 
 surface = pygame.display.set_mode((600, 400))
-click = pygame.mixer.Sound('data/Bat_2.wav')
+click = pygame.mixer.Sound('Bat_2.wav')
 click.set_volume(0.2)
 
 engine = sound.Sound()
-engine.set_sound(sound.SOUND_TYPE_KEY_ADDITION, 'data/Bat_2.wav')
+engine.set_sound(sound.SOUND_TYPE_KEY_ADDITION, 'Bat_2.wav')
 
-pygame.mixer.music.load('data/menu.mp3')
+pygame.mixer.music.load('menu.mp3')
 pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(0.2)
 
@@ -46,13 +47,11 @@ mytheme.background_color = (51, 51, 51)
 menu = pygame_menu.Menu('Space Invaders', 600, 400,
                         theme=mytheme)
 # ------------------------------
-user_name = menu.add.text_input('Name:', default=' ', font_color=(240, 230, 140))
 
+user_name = menu.add.text_input('Name:', default=' ', font_color=(240, 230, 140))
 menu.add.selector('difficulty:', [('Hard', 1), ('Easy', 3)], onchange=set_difficulty, font_color=(240, 230, 140))
-menu.add.button('Sound', font_color=(240, 230, 140))
+menu.add.button('Sound', Mixers.sounder, font_color=(240, 230, 140))
 menu.add.button('Play!', start_the_game, font_color=(240, 230, 140))
 menu.add.button('Exit', pygame_menu.events.EXIT, font_color=(240, 230, 140))
-
 menu.set_sound(engine, recursive=True)
-
 menu.mainloop(surface)
