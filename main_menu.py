01c11fe9d0192874d50a8ac2e61d51d3eb6acb49
Zarikato <91799879+Zarikato@users.noreply.github.com>
2022-02-11 20:05:29 +0400

    Update main_menu.py
    исправлены звуки

--- main_menu.py
+++ main_menu.py
@@ -1,19 +1,22 @@
 import pygame
 import pygame_menu
+from pygame_menu import sound
 from typing import Tuple, Any
 from space import Hp
 
 pygame.init()
 pygame.display.set_caption('Space Invaders')
-programIcon = pygame.image.load('icon.png')
+programIcon = pygame.image.load('data/icon.png')
 pygame.display.set_icon(programIcon)
 
 surface = pygame.display.set_mode((600, 400))
-click = pygame.mixer.Sound('Bat_2.wav')
+click = pygame.mixer.Sound('data/Bat_2.wav')
 click.set_volume(0.2)
 
+engine = sound.Sound()
+engine.set_sound(sound.SOUND_TYPE_KEY_ADDITION, 'data/Bat_2.wav')
 
-pygame.mixer.music.load('menu.mp3')
+pygame.mixer.music.load('data/menu.mp3')
 pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(0.2)
 
@@ -43,23 +46,13 @@ mytheme.background_color = (51, 51, 51)
 menu = pygame_menu.Menu('Space Invaders', 600, 400,
                         theme=mytheme)
 # ------------------------------
-
-# clock = pygame.time.Clock()
-# done = False
-# while not done:
-#     for event in pygame.event.get():
-#         if event.type == pygame.QUIT:
-#             done = True
-#     keys = pygame.key.get_pressed()
-#     if keys[pygame.K_UP] or keys[pygame.K_DOWN]:
-#         click.play()
-#         pygame.display.flip()
-
 user_name = menu.add.text_input('Name:', default=' ', font_color=(240, 230, 140))
 
 menu.add.selector('difficulty:', [('Hard', 1), ('Easy', 3)], onchange=set_difficulty, font_color=(240, 230, 140))
 menu.add.button('Sound', font_color=(240, 230, 140))
 menu.add.button('Play!', start_the_game, font_color=(240, 230, 140))
 menu.add.button('Exit', pygame_menu.events.EXIT, font_color=(240, 230, 140))
+
+menu.set_sound(engine, recursive=True)
 
 menu.mainloop(surface)
