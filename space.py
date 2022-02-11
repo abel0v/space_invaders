0c5e001a740332ae71c18b47dfc64a7b86d342b6
Larom4ik <76567719+Larom4ik@users.noreply.github.com>
2022-02-11 19:36:13 +0400

    Update space.py

--- space.py
+++ space.py
@@ -6,24 +6,21 @@ from pygame.sprite import Group
 from monsters import Monsters
 
 
+class Hp(pygame.sprite.Sprite):
+    def start():
+        pygame.init()
+        pygame.display.set_caption('space')
+        screen = pygame.display.set_mode((1200, 800))
 
-def start():
-    pygame.init()
-    pygame.display.set_caption('space')
-    screen = pygame.display.set_mode((1200, 800))
-    background = (0, 0, 0)
-    gun = Gun(screen)
-    bullets = Group()
-    monsters = Group()
-    monsters2 = Group()
-    controller.create_army(screen, monsters)
+        background = (0, 0, 0)
+        gun = Gun(screen)
+        bullets = Group()
+        monsters = Group()
+        monsters2 = Group()
+        controller.create_army(screen, monsters)
 
-
-    while True:
-        controller.events(screen, gun, bullets)
-        controller.screen_update(background, screen, gun, monsters, monsters2, bullets)
-        bullets.update()
-        gun.gun_update()
-
-
-start()
+        while True:
+            controller.events(screen, gun, bullets)
+            controller.screen_update(background, screen, gun, monsters, monsters2, bullets)
+            bullets.update()
+            gun.gun_update()
