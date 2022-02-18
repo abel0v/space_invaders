0073753c4bbdcc9b510df87aa0fd481577ad5380
Larom4ik <76567719+Larom4ik@users.noreply.github.com>
2022-02-18 16:02:34 +0400

    Update main_menu.py

--- main_menu.py
+++ main_menu.py
@@ -16,8 +16,9 @@ click.set_volume(0.2)
 
 engine = sound.Sound()
 engine.set_sound(sound.SOUND_TYPE_KEY_ADDITION, 'data/Bat_2.wav')
+engine.set_sound(sound.SOUND_TYPE_KEY_DELETION, 'data/Button_2.wav')
 
-pygame.mixer.music.load('data/menu.mp3')
+pygame.mixer.music.load('menu.mp3')
 pygame.mixer.music.play(-1)
 pygame.mixer.music.set_volume(0.2)
 
@@ -33,7 +34,6 @@ def start_the_game():
     print(f'{user_name.get_value()}, Do the job here!')
 
     space.start()
-    # если кинуть что-нибудь сюда, оно откроется в отдельном окне, тут будет основная игра
 
 
 # -------------тема---------
@@ -46,13 +46,15 @@ mytheme.title_background_color = (72, 61, 139)
 mytheme.background_color = (51, 51, 51)
 menu = pygame_menu.Menu('Space Invaders', 600, 400,
                         theme=mytheme)
-
 # ------------------------------
 
-
-user_name = menu.add.text_input('Name:', default=' ', font_color=(240, 230, 140))
+user_name = menu.add.text_input('Name: ', default='', font_color=(240, 230, 140), shadow=True)
+menu.add.vertical_margin(5)
 menu.add.selector('difficulty:', [('Hard', 1), ('Easy', 3)], onchange=set_difficulty, font_color=(240, 230, 140))
+menu.add.vertical_margin(5)
 menu.add.button('Play!', start_the_game, font_color=(240, 230, 140))
+menu.add.vertical_margin(10)
 menu.add.button('Exit', pygame_menu.events.EXIT, font_color=(240, 230, 140))
+
 menu.set_sound(engine, recursive=True)
 menu.mainloop(surface)
